3b7a146a825069cc51048600dd7eeaf5afbd6eeb
Dustin Lacewell <dlacewell@gmail.com>
2025-04-24 08:37:16 -0500

    feat: Add colors to the log with Chalk

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,21 +1,26 @@
+import chalk from "chalk"
+
 export class Logger {
     constructor(public dryRun: boolean = false, public verbose: boolean = false) {}
 
-    info(message: string): void {
-        console.log(`[INFO] ${message}`)
+    public info(message: string): void {
+        console.log(`${chalk.blue("[INFO]")} ${message}`)
     }
 
-    extra(message: string): void {
+    public extra(message: string): void {
         if (this.verbose) {
-            console.log(`[VERBOSE] ${message}`)
+            console.log(`${chalk.magenta("[VERBOSE]")} ${message}`)
         }
     }
 
-    action(message: string): void {
-        console.log(`[${this.dryRun ? "WOULD " : ""}ACTION] ${message}`)
+    public action(message: string): void {
+        const label: string = this.dryRun
+            ? chalk.yellow("[WOULD ACTION]")
+            : chalk.green("[ACTION]")
+        console.log(`${label} ${message}`)
     }
 
-    error(message: string): void {
-        console.error(`[ERROR] ${message}`)
+    public error(message: string): void {
+        console.error(`${chalk.red("[ERROR]")} ${message}`)
     }
 }
