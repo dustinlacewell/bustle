d269a816cf499a4d702b1a6c261f71790331ebff
Dustin Lacewell <dlacewell@gmail.com>
2025-04-24 20:26:49 -0500

    Check file access while zipping

--- src/commands/zip/createModZip.ts
+++ src/commands/zip/createModZip.ts
@@ -1,19 +1,34 @@
-import * as fs from 'fs/promises'
-import path from 'path'
-
-import AdmZip from 'adm-zip'
+import AdmZip from "adm-zip"
+import * as fs from "fs/promises"
+import path from "path"
 
 import { Logger } from "@/lib/logger.js"
-import { checkDestSafety } from '@/lib/path-utils.js'
+import { checkDestSafety } from "@/lib/path-utils.js"
 
 export async function createModZip(from: string, to: string, logger: Logger): Promise<void> {
-    checkDestSafety(from, to, ['from', 'to'])
+    checkDestSafety(from, to, ["from", "to"])
+
+    try {
+        await fs.access(from)
+    }
+    catch (error) {
+        console.error("Error:", error instanceof Error ? error.message : error)
+        process.exit(1)
+    }
+
+    try {
+        await fs.access(to)
+    }
+    catch (error) {
+        console.error("Error:", error instanceof Error ? error.message : error)
+        process.exit(1)
+    }
 
     logger.action(`Creating zip archive: ${to} from ${from}`)
 
     if (!logger.dryRun) {
         const zip = new AdmZip()
-        
+
         // find all files and folders in from
         const entries = await fs.readdir(from, { withFileTypes: true })
         for (const entry of entries) {
@@ -26,7 +41,7 @@ export async function createModZip(from: string, to: string, logger: Logger): Pr
                 zip.addLocalFile(path.join(from, entry.name), entry.name)
             }
         }
-        
+
         await new Promise<void>((resolve, reject) => {
             logger.action(`Writing zip file: ${to}`)
             zip.writeZip(to, (error) => {
